980a1fcf803df28cf185b602b6731925d46ece92
Andres <apilapanta@gmail.com>
2025-11-17 07:21:59 -0500

    Lectura de MONGO_URI desde variables de entorno

--- database.py
+++ database.py
@@ -1,7 +1,15 @@
 from pymongo import MongoClient
+import os
 
-MONGO_URI = "mongodb+srv://kuska:12345@kuska.eqrybhg.mongodb.net/?retryWrites=true&w=majority&appName=kuska"
+# Lee la clave MONGO_URI de las variables de entorno (Render la inyectará)
+MONGO_URI = os.environ.get('MONGO_URI') 
 
+if MONGO_URI is None:
+    # Si no encuentra la variable, lanza un error (solo ocurriría si Render falla)
+    raise Exception("La variable MONGO_URI no está configurada. Verifica el panel de Render.")
+
+# Ahora usa la URI para conectarte
 client = MongoClient(MONGO_URI)
+db = client['el_nombre_de_tu_db'] # ¡Asegúrate de poner el nombre de tu base de datos aquí!
 
-db = client["kuska"]  # nombre de la base de datos
+# Si usas Blueprints o si el código continúa en app.py, asegúrate de que use 'client' o 'db'
